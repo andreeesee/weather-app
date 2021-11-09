2e91551b38d32cbcb8cf18bc313302cce99a2860
See <john.andre.b.see@accenture.com>
2021-11-09 14:10:30 +0800

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require("request")
+
+const forecast = (long, lat, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=44f403efb68b1b7bba06395231c3b263&query='+ long +','+ lat +'&units=f'
+    request({ url, json: true}, (error, {body} ) => {
+        if(error){
+            callback('Unable to connect to weather services', undefined)
+        }else if(body.success === false){
+            callback(response.body.error.info, undefined);
+        }  else {
+            const temp = body.current.temperature
+            const feelsLike = body.current.feelslike
+            const weatherDesc = body.current.weather_descriptions[0]
+            
+            callback(undefined, `${weatherDesc}, It is currently ${temp} but it feels like ${feelsLike}`)            
+        }
+    })
+
+}
+
+module.exports = forecast
